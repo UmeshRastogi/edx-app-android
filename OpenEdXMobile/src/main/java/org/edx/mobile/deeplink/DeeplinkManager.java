ab6ee3aa88df75a2e49d34f5d4a5bd26f92ddb8d
Mian Khalid <mkhalid@edx.org>
2019-02-26 18:36:03 +0500

    Implement Course Dashboard deeplink

---
+++ OpenEdXMobile/src/main/java/org/edx/mobile/deeplink/DeeplinkManager.java
@@ -0,0 +1,34 @@
+package org.edx.mobile.deeplink;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+
+import org.edx.mobile.logger.Logger;
+import org.edx.mobile.module.prefs.LoginPrefs;
+import org.edx.mobile.view.Router;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class DeeplinkManager {
+    protected static final Logger logger = new Logger(DeeplinkManager.class);
+    public static final String KEY_CLICKED_BRANCH_LINK = "+clicked_branch_link";
+    private static final String KEY_SCREEN_NAME = "screen_name";
+    private static final String KEY_COURSE_ID = "course_id";
+
+    public static void parseAndReact(@NonNull Activity activity, @NonNull JSONObject paramsJson) throws JSONException {
+        logger.debug("Deeplink received. JSON Details:\n" + paramsJson);
+        final Router router = new Router();
+        final boolean isLoggedIn = new LoginPrefs(activity).getUsername() != null;
+        if (!isLoggedIn) {
+            activity.startActivity(router.getLogInIntent());
+        } else {
+            final String screenName = paramsJson.getString(KEY_SCREEN_NAME);
+            switch (screenName) {
+                case "course_dashboard":
+                    final String courseId = paramsJson.getString(KEY_COURSE_ID);
+                    router.showCourseDashboardTabs(activity, null, courseId, false);
+                    break;
+            }
+        }
+    }
+}
