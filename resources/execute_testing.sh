345f55261dd222f21c6c9277afc52f14e0bf1b96
Naveed <nsaleem@edx.org>
2019-02-27 15:29:56 +0500

    Fixed points highlighted

--- resources/execute_testing.sh
+++ resources/execute_testing.sh
@@ -12,10 +12,10 @@ sleep 1m
 $ANDROID_HOME/platform-tools/adb shell pm list packages | grep org.edx.mobile
 if [ $? == 0 ]; then
    echo "edX app is installed successfully"
-   #  ==============WIP==========
-   # Install dependencies 
-   exit 0
 else
    echo "edX app is installed successfully"
    exit 1
 fi
+
+#  ==============WIP==========
+# Install dependencies 
